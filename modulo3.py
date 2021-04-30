ce210c32713103a310f98e6325420cf770753ccf
ramy <ramses-c14@live.com.mx>
2021-04-30 16:49:34 -0500

    Se continua con el módulo 3

--- modulo3.py
+++ modulo3.py
@@ -169,3 +169,63 @@ elif x == y: #No entra ya que el anterior if es correcto
     print("tres")
 else: #Tampoco entra
     print("cuatro")
+
+#Ciclo While
+# Almacenaremos el número más grande actual aquí
+numeroMayor = -999999999
+
+# Ingresa el primer valor
+numero = int(input ("Introduzca un número o escriba -1 para detener:"))
+
+# Si el número no es igual a -1, continuaremos
+while numero != -1:
+   # ¿Es el número más grande que el número más grande?
+   if numero > numeroMayor:
+       # Sí si, actualiza el mayor númeroNúmero
+       numeroMayor = numero
+   # Ingresa el siguiente número
+   numero = int (input("Introduce un número o escribe -1 para detener:"))
+
+# Imprimir el número más grande
+print("El número más grande es:", numeroMayor)
+
+#Ejercicio ciclo while
+# programa que lee una secuencia de números
+# y cuenta cuántos números son pares y cuántos son impares
+# programa termina cuando se ingresa cero
+
+numerosImpares = 0
+numerosPares = 0
+
+# lee el primer número
+numero = int (input ("Introduce un número o escriba 0 para detener:"))
+
+# 0 termina la ejecución
+while numero:
+    # verificar si el número es impar
+    if numero % 2:
+        # aumentar el contador de números impares
+        numerosImpares += 1
+    else:
+        # aumentar el contador de números pares
+        numerosPares += 1
+    # lee el siguiente número
+    numero = int (input ("Introduce un número o escriba 0 para detener:"))
+
+# imprimir resultados
+print("Números impares: ", numerosImpares)
+print("Números pares: ", numerosPares)
+
+#Diferentes formas de utilizar el while
+contador = 5
+while contador != 0:
+    print("Dentro del ciclo: ", contador)
+    contador -= 1
+print("Fuera del ciclo", contador)
+
+contador = 5
+while contador:
+    print("Dentro del ciclo.", contador)
+    contador -= 1
+print("Fuera del ciclo", contador)
+
