2769325688d72bdef0e9d3caafc9e53f32fe03b0
ramy <ramses-c14@live.com.mx>
2021-05-14 12:43:14 -0500

    Arreglos multidimensionales y fin del módulo 3

--- modulo3.py
+++ modulo3.py
@@ -810,3 +810,13 @@ tablero[3][4] = PEON
 
 print(tablero)
 
+#Arreglos Tridimensionales
+habitaciones =[[[False for r in range(20)] for f in range(15)] for t in range(3)]
+habitaciones[1][9][13] = True
+habitaciones[0][4][1] = False
+
+vacante = 0
+
+for numeroHabitacion in range(20):
+    if not habitaciones[2][14][numeroHabitacion]:
+        vacante += 1
