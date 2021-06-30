661277b7340d041f4c464cb34d538f39c9907819
ramy <ramses-c14@live.com.mx>
2021-06-30 01:17:28 -0500

    Pequeños cambios

--- modulo4.py
+++ modulo4.py
@@ -491,3 +491,12 @@ print(dict)
 print(numerosTelefono)
 print(diccionarioVacio)
 
+dict = {"gato" : "chat", "perro" : "chien", "caballo" : "cheval"}
+words = ['gato', 'leon', 'caballo']
+
+for word in words:
+    if word in dict:
+        print(word, "->", dict[word])
+    else:
+        print(word, "no está en el diccionario")
+
