53a5a3bf571801ab81c3568beed5598bca9be404
Ramsés Castro <ramses-c14@live.com.mx>
2021-08-25 21:49:48 -0500

    Más cadenas

--- Clases/modulo5.py
+++ Clases/modulo5.py
@@ -356,39 +356,39 @@ t = [0, 1, 2]
 print(min(t)) # 0
 
 # Demostrando max() - Ejemplo 1
-print(max("aAbByYzZ")) 
+print(max("aAbByYzZ")) # z
 
 
 # Demonstrando max() - Examplos 2 y 3
 t = 'Los Caballeros Que Dicen "¡Ni!"'
-print('[' + max(t) + ']')
+print('[' + max(t) + ']') # [ ]
 
 t = [0, 1, 2]
-print(max(t))
+print(max(t)) # 2
 
 # Demonstrando el método index()
-print("aAbByYzZaA".index("b"))
-print("aAbByYzZaA".index("Z"))
-print("aAbByYzZaA".index("A"))
+print("aAbByYzZaA".index("b")) # 2
+print("aAbByYzZaA".index("Z")) # 7
+print("aAbByYzZaA".index("A")) # 1
 
 # Demostrando la función list()
-print(list("abcabc"))
+print(list("abcabc")) # ['a', 'b', 'c']
 
 # Demostrando el método count()
-print("abcabc".count("b"))
-print('abcabc'.count("d"))
+print("abcabc".count("b")) # 2
+print('abcabc'.count("d")) # 0
 
 # Demostración del método capitalize()
-print('aBcD'.capitalize())
-print('123'.capitalize())
-print("αβγδ".capitalize())
+print('aBcD'.capitalize()) # Abcd
+print('123'.capitalize()) # 123
+print("αβγδ".capitalize()) # Αβγδ
 
 # Demostración del método center()
-print('[' + 'alfa'.center(10) + ']')
-print('[' + 'Beta'.center(2) + ']')
-print('[' + 'Beta'.center(4) + ']')
-print('[' + 'Beta'.center(6) + ']')
-print('[' + 'gamma'.center(20, '*') + ']')
+print('[' + 'alfa'.center(10) + ']') # [alfa]
+print('[' + 'Beta'.center(2) + ']') # [Beta]
+print('[' + 'Beta'.center(4) + ']') # [Beta]
+print('[' + 'Beta'.center(6) + ']') # [Beta]
+print('[' + 'gamma'.center(20, '*') + ']') # [gamma********]
 
 # Demostración del método endswith()
 if "epsilon".endswith("on"):
@@ -397,16 +397,74 @@ else:
     print("no")
     
 t = "zeta"
-print(t.endswith("a"))
-print(t.endswith("A"))
-print(t.endswith("et"))
-print(t.endswith("eta"))
+print(t.endswith("a")) # False
+print(t.endswith("A")) # True
+print(t.endswith("et")) # False
+print(t.endswith("eta")) # True
 
 # Demostración del método find()
-print("Eta".find("ta"))
-print("Eta".find("mma"))
-print('kappa'.find('a', 2))
+print("Eta".find("ta")) # 2
+print("Eta".find("mma")) # -1
+print('kappa'.find('a', 2)) # 2
 
-print('kappa'.find('a', 1, 4))
-print('kappa'.find('a', 2, 4))
+print('kappa'.find('a', 1, 4)) # 3
+print('kappa'.find('a', 2, 4)) # -1
+
+# Demostración del método the isalnum()
+print('lambda30'.isalnum()) # True
+print('lambda'.isalnum()) # False
+print('30'.isalnum()) # False
+print('@'.isalnum()) # False
+print('lambda_30'.isalnum()) # False
+print(''.isalnum()) # False
+
+t = 'Six lambdas'
+print(t.isalnum()) # True
+
+t = 'ΑβΓδ'
+print(t.isalnum()) # True
+
+t = '20E1'
+print(t.isalnum()) # False
+
+# Ejemplo 1: Demostración del método isapha() letras
+print("Moooo".isalpha()) # False
+print('Mu40'.isalpha()) # True
+
+# Ejemplo 2: Demostración del método isdigit() numeros
+    
+print('2018'.isdigit()) # True
+print("Año2019".isdigit()) # False
+
+# Ejemplo 1: Demostración del método islower() minusculas
+print("Moooo".islower()) # True
+print('moooo'.islower()) # True
+
+# Ejemplo 2: Demostración del método isspace() espacio
+print(' \n '.isspace()) # True
+print(" ".isspace()) # True
+print("mooo mooo mooo".isspace()) # False
+
+# Ejemplo 3: Demostración del método isupper() mayusculas
+print("Moooo".isupper()) # False
+print('moooo'.isupper()) # False
+print('MOOOO'.isupper()) # True
+
+# Demostración del método join()
+print(",".join(["omicron", "pi", "rho"])) # omicron,pi,rho
+
+# Demostración del método lower() pasa las mayusculas a minusculas
+print("SiGmA=60".lower()) # sigma=60
+
+# Demostración del método the lstrip()
+print("[" + " tau ".lstrip() + "]") # [ tau ]
+print("www.cisco.com".lstrip("w.")) # ww.cisco.com
+print("pythoninstitute.org".lstrip(".org")) # pythoninstitute
+
+# Demostración del método replace()
+print("www.netacad.com".replace("netacad.com", "pythoninstitute.org")) # www.pythoninstitute.org
+print("This is it!".replace("is", "are")) # This are it!
+print("Apple juice".replace("juice", "")) # Apple
+print("This is it!".replace("is", "are", 1)) # This are it!
+print("This is it!".replace("is", "are", 2)) # This are it!
 
